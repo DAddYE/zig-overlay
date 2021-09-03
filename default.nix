af4bcc264aea228c974c400daba14a1c30486feb
Davide D'Agostino <info@daddye.it>
2021-09-03 10:46:37 -0700

    Fix issues on darwin while copying docs
    Seems that different versions of nix have a slightly different name for docs:
    
    - 0.6.x version uses `doc`
    - 0.7.x version uses `docs`
    - 0.8.x version uses `doc`
    - 0.9.x version uses `docs`
    
    To make it a bit more futureprof I'm now just checking if the dir exist before copying because on macos for instance, if the dir doesn't exist the build will fail.

--- default.nix
+++ default.nix
@@ -3,28 +3,10 @@
 
 let inherit (pkgs) lib;
     releases = builtins.fromJSON (lib.strings.fileContents ./sources.json);
+
 in lib.attrsets.mapAttrs (k: v: 
-  if k == "master" then
-    lib.attrsets.mapAttrs (k: v:
-      (pkgs.stdenv.mkDerivation {
-        pname = "zig";
-        inherit (v.${system}) version;
-        src = pkgs.fetchurl {
-          inherit (v.${system}) url sha256;
-        };
-        dontConfigure = true;
-        dontBuild = true;
-        dontFixup = true;
-        installPhase = ''
-          mkdir -p $out/{doc,bin,lib}
-          cp -r docs/* $out/doc
-          cp -r lib/* $out/lib
-          cp zig $out/bin/zig
-        '';
-      }))
-      v
-  else
-    pkgs.stdenv.mkDerivation {
+  lib.attrsets.mapAttrs (k: v:
+    (pkgs.stdenv.mkDerivation {
       pname = "zig";
       inherit (v.${system}) version;
       src = pkgs.fetchurl {
@@ -35,12 +17,11 @@ in lib.attrsets.mapAttrs (k: v:
       dontFixup = true;
       installPhase = ''
         mkdir -p $out/{doc,bin,lib}
-        cp -r ${if k == "0.6.0" then "doc/*"
-                else
-                  if k == "0.7.0" then "langref.html"
-                  else "docs/*"} $out/doc
+        [ -d docs ] && cp -r docs/* $out/doc
+        [ -d doc ] && cp -r doc/* $out/doc
         cp -r lib/* $out/lib
         cp zig $out/bin/zig
       '';
-    })
+    }))
+    v
   releases
