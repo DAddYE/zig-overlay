39d034932f72c5e66f290275bde65df1a57c9e86
Davide D'Agostino <info@daddye.it>
2021-09-03 11:04:52 -0700

    Restore changes between master and stable.

--- default.nix
+++ default.nix
@@ -2,11 +2,32 @@
   system ? builtins.currentSystem }:
 
 let inherit (pkgs) lib;
-    releases = builtins.fromJSON (lib.strings.fileContents ./sources.json);
+  releases = builtins.fromJSON (lib.strings.fileContents ./sources.json);
+  installPhase = ''
+    mkdir -p $out/{doc,bin,lib}
+    [ -d docs ] && cp -r docs/* $out/doc
+    [ -d doc ] && cp -r doc/* $out/doc
+    cp -r lib/* $out/lib
+    cp zig $out/bin/zig
+  '';
 
 in lib.attrsets.mapAttrs (k: v: 
-  lib.attrsets.mapAttrs (k: v:
-    (pkgs.stdenv.mkDerivation {
+  if k == "master" then
+    lib.attrsets.mapAttrs (k: v:
+      (pkgs.stdenv.mkDerivation {
+        pname = "zig";
+        inherit (v.${system}) version;
+        src = pkgs.fetchurl {
+          inherit (v.${system}) url sha256;
+        };
+        dontConfigure = true;
+        dontBuild = true;
+        dontFixup = true;
+        installPhase = installPhase;
+      }))
+      v
+  else
+    pkgs.stdenv.mkDerivation {
       pname = "zig";
       inherit (v.${system}) version;
       src = pkgs.fetchurl {
@@ -15,13 +36,6 @@ in lib.attrsets.mapAttrs (k: v:
       dontConfigure = true;
       dontBuild = true;
       dontFixup = true;
-      installPhase = ''
-        mkdir -p $out/{doc,bin,lib}
-        [ -d docs ] && cp -r docs/* $out/doc
-        [ -d doc ] && cp -r doc/* $out/doc
-        cp -r lib/* $out/lib
-        cp zig $out/bin/zig
-      '';
-    }))
-    v
+      installPhase = installPhase;
+    })
   releases
